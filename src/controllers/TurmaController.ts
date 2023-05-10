16e2d4e72ce085aad18cd8562f9a63909c2131bc
Felipe Duaibs <felipe.duaibs@hotmail.com>
2023-05-09 23:22:20 -0300

    fix: pdf fields

--- src/controllers/TurmaController.ts
+++ src/controllers/TurmaController.ts
@@ -23,14 +23,75 @@ export class TurmaController {
       orderBy: { dataProgramada: 'asc' },
     });
 
-    if (aulas.length <= 0) {
+    const turma = await prisma.turma.findUnique({
+      select: {
+        professorId: true,
+        semestreId: true,
+        disciplinaId: true,
+      },
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!turma) {
       res.status(404).json({
         error: true,
-        message: 'Nenhuma aula encontrada para a turma escolhida',
+        message: `Nenhuma turma com o id ${id} encontrada`,
       });
     }
 
-    const pdf = await pdfCreator.create({ aulas: aulas, teste: 'teste' });
+    const professor = await prisma.professor.findUnique({
+      select: {
+        nome: true,
+      },
+      where: {
+        id: turma?.professorId,
+      },
+    });
+
+    const semestre = await prisma.semestre.findUnique({
+      select: {
+        sigla: true,
+      },
+      where: {
+        id: turma?.semestreId,
+      },
+    });
+
+    const disciplina = await prisma.disciplina.findUnique({
+      select: {
+        nome: true,
+      },
+      where: {
+        id: turma?.disciplinaId,
+      },
+    });
+
+    if (aulas.length <= 0 || !professor || !semestre || !disciplina) {
+      res.status(404).json({
+        error: true,
+        message: 'Falha ao buscar dados para gerar o relatório',
+      });
+    }
+
+    const aulasFormatadas = aulas.map((aula) => {
+      const { dataProgramada, ...resto } = aula;
+
+      const dataFormatada = dataProgramada.toLocaleDateString('pt-BR');
+
+      return {
+        ...resto,
+        dataProgramada: dataFormatada,
+      };
+    });
+
+    const pdf = await pdfCreator.create({
+      aulas: aulasFormatadas,
+      professor,
+      semestre,
+      disciplina,
+    });
 
     if (pdf.length <= 0) {
       res.status(500).json({ error: true, message: 'Falha ao criar PDF' });
